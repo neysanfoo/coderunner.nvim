8da5efdc4fd9a49c73589b6e8aea6c66abf63804
neysanfoo <neysanfoo@gmail.com>
2024-11-13 14:59:06 +0800

    fix bug

--- lua/coderunner/core.lua
+++ lua/coderunner/core.lua
@@ -9,87 +9,68 @@ function M.setup(config)
 		return path:gsub("\\", "/") -- Convert Windows paths to Unix style
 	end
 
-	-- Function to run a command based on filetype
-	local function run_filetype_command()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-		local command = config.filetype_commands[filetype]
-		if not command then
-			print("Unsupported filetype: " .. filetype)
-			return
-		end
-
-		local filename = vim.api.nvim_buf_get_name(bufnr)
-		local dir = shell_escape(vim.fn.fnamemodify(filename, ":p:h"))
-		local fileNameWithoutExt = shell_escape(vim.fn.fnamemodify(filename, ":p:t:r"))
-		local fullFilePath = shell_escape(vim.fn.fnamemodify(filename, ":p"))
-
-		if type(command) == "table" then
-			for i, cmd in ipairs(command) do
-				command[i] = cmd:gsub("$dir", dir)
-						:gsub("$fullFilePath", fullFilePath)
-						:gsub("$fileNameWithoutExt", fileNameWithoutExt)
-			end
-			command = table.concat(command, " && ")
-		else
-			command = command
-					:gsub("$dir", dir)
-					:gsub("$fullFilePath", fullFilePath)
-					:gsub("$fileNameWithoutExt", fileNameWithoutExt)
-		end
-
-		-- Debug line
-		print(command)
-
-		return command
-	end
-
-	-- Function to clean up the terminal window and buffer
-	local function cleanup_terminal()
+	vim.api.nvim_create_user_command("Run", function()
+		-- Clean up previous window and buffer
 		if output_win_buf.winid and vim.api.nvim_win_is_valid(output_win_buf.winid) then
-			-- Close the window first
 			vim.api.nvim_win_close(output_win_buf.winid, true)
 			output_win_buf.winid = nil
 		end
 
 		if output_win_buf.bufnr and vim.api.nvim_buf_is_valid(output_win_buf.bufnr) then
-			-- Force delete the buffer
 			vim.api.nvim_buf_delete(output_win_buf.bufnr, { force = true })
 			output_win_buf.bufnr = nil
 		end
 
-		-- Small delay to ensure cleanup is complete
-		vim.cmd("sleep 50m")
-	end
+		-- Get current buffer info first
+		local current_buf = vim.api.nvim_get_current_buf()
+		local filetype = vim.api.nvim_buf_get_option(current_buf, "filetype")
+		local command = config.filetype_commands[filetype]
 
-	vim.api.nvim_create_user_command("Run", function()
-		-- Always cleanup previous terminal
-		cleanup_terminal()
-
-		-- Get the command to run based on the filetype
-		local command = run_filetype_command()
 		if not command then
+			print("Unsupported filetype: " .. filetype)
 			return
 		end
 
+		-- Get file information
+		local filename = vim.api.nvim_buf_get_name(current_buf)
+		local dir = shell_escape(vim.fn.fnamemodify(filename, ":p:h"))
+		local fileNameWithoutExt = shell_escape(vim.fn.fnamemodify(filename, ":p:t:r"))
+		local fullFilePath = shell_escape(vim.fn.fnamemodify(filename, ":p"))
+
+		-- Process command
+		local final_command
+		if type(command) == "table" then
+			local processed_commands = {}
+			for _, cmd in ipairs(command) do
+				local processed_cmd = cmd:gsub("$dir", dir)
+					:gsub("$fullFilePath", fullFilePath)
+					:gsub("$fileNameWithoutExt", fileNameWithoutExt)
+				table.insert(processed_commands, processed_cmd)
+			end
+			final_command = table.concat(processed_commands, " && ")
+		else
+			final_command = command
+				:gsub("$dir", dir)
+				:gsub("$fullFilePath", fullFilePath)
+				:gsub("$fileNameWithoutExt", fileNameWithoutExt)
+		end
+
+		-- Debug line
+		-- print(final_command)
+
+		-- Create new terminal window
 		local height = vim.api.nvim_get_option("lines")
 		local split_size = math.min(config.buffer_height, height)
 		local current_win = vim.api.nvim_get_current_win()
 
-		-- Create new window and set it up
 		vim.cmd(split_size .. "new")
 		local output_win = vim.api.nvim_get_current_win()
 
-		-- Start terminal with command
-		local job_id = vim.fn.termopen(command, {
-			on_exit = function(_, exit_code)
-				if exit_code ~= 0 then
-					-- Optional: Handle non-zero exit codes
-				end
-			end,
-		})
-
+		-- Run the command
+		vim.fn.termopen(final_command)
 		local output_bufnr = vim.api.nvim_win_get_buf(output_win)
+
+		-- Store window and buffer info
 		output_win_buf.winid = output_win
 		output_win_buf.bufnr = output_bufnr
 
